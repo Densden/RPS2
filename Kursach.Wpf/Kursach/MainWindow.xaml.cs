d647b8b21ad25649e96a1e4284c8d1f3c2871908
denshifu <denshifu@mail.ru>
2018-12-07 00:12:51 +0300

    1

--- Kursach.Wpf/Kursach/MainWindow.xaml.cs
+++ Kursach.Wpf/Kursach/MainWindow.xaml.cs
@@ -48,11 +48,7 @@ namespace Kursach
            ResultText.Text = Test.GetAnswer().Replace("\0", "");
         }
 
-        //private void Button_Click_1(object sender, RoutedEventArgs e)
-        //{
-        //    avtiraz avtiraz = new avtiraz();
-        //    avtiraz.Show();
-        //}
+       
     }
 }
         
